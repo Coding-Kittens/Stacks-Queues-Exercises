7a0f0e8adc69a30a338b06150c5d9b2e7e6a80c5
Coding-Kittens <105325005+Coding-Kittens@users.noreply.github.com>
2023-01-11 22:19:36 -0500

    first commit

---
+++ stack.js
@@ -0,0 +1,52 @@
+/** Node: node for a stack. */
+
+class Node {
+  constructor(val, next = null) {
+    this.val = val;
+    this.next = next;
+  }
+}
+
+/** Stack: chained-together nodes where you can
+ *  remove from the top or add to the top. */
+
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  /** push(val): add new value to end of the stack. Returns undefined. */
+
+  push(val) {
+    this.first = new Node(val, this.first);
+    if (this.isEmpty()) this.last = this.first;
+    this.size++;
+  }
+
+  /** pop(): remove the node from the top of the stack
+   * and return its value. Should throw an error if the stack is empty. */
+
+  pop() {
+    if (this.isEmpty()) throw new Error();
+    this.size--;
+    let node = this.first;
+    this.first = node.next;
+    return node.val;
+  }
+
+  /** peek(): return the value of the first node in the stack. */
+
+  peek() {
+    return this.first.val;
+  }
+
+  /** isEmpty(): return true if the stack is empty, otherwise false */
+
+  isEmpty() {
+    return this.size <= 0;
+  }
+}
+
+module.exports = Stack;
